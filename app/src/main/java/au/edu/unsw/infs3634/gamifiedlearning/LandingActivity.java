5181fcb9a5c0aa830aa33caa8ead43f968240117
UNSWConnor <z5189800@ad.unsw.edu.au>
2020-11-06 16:28:32 +1100

    Second commit Added icons Added landing screen Added String resources Changed app style

---
+++ app/src/main/java/au/edu/unsw/infs3634/gamifiedlearning/LandingActivity.java
@@ -0,0 +1,50 @@
+package au.edu.unsw.infs3634.gamifiedlearning;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.ScaleAnimation;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class LandingActivity extends AppCompatActivity {
+    private ImageView ivIcon;
+    private Button bLogin, bRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing);
+
+        ivIcon = findViewById(R.id.ivIcon);
+        bLogin = findViewById(R.id.bLogin);
+        bRegister = findViewById(R.id.bRegister);
+
+        bLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchLoginActivity();
+            }
+        });
+
+        bRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchRegisterActivity();
+            }
+        });
+    }
+
+    private void launchLoginActivity() {
+        Intent loginIntent = new Intent(this, LoginScreen.class);
+        startActivity(loginIntent);
+    }
+
+    private void launchRegisterActivity() {
+        Intent registerIntent = new Intent(this, RegisterScreen.class);
+        startActivity(registerIntent);
+    }
+}
